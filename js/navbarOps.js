40d462ea00aa729e36df834b29fefbce5570f797
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-05-05 17:47:20 +0530

    uploading navbbar operations script

---
+++ js/navbarOps.js
@@ -0,0 +1,28 @@
+//Script for Navbar operations
+//Ensureing DOMContent is loaded
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Function to switch theme
+    function switchTheme() {
+
+        //Checking if the theme is dark
+        if (getComputedStyle(document.documentElement).getPropertyValue('--primary-color').trim() == '#1f1f1f') {
+            document.documentElement.style.setProperty('--primary-color', '#e0e0e0')
+            document.documentElement.style.setProperty('--secondary-color', '#1f1f1f')
+
+            //Switching the theme icon to 'dark'
+            document.getElementById('theme-toggle').innerHTML = `<i class='bx bx-moon'></i>`
+        }
+        else {
+            document.documentElement.style.setProperty('--primary-color', '#1f1f1f')
+            document.documentElement.style.setProperty('--secondary-color', '#e0e0e0')
+
+            //Switching the theme icon to 'ligt'
+            document.getElementById('theme-toggle').innerHTML = `<i class='bx bx-sun'></i>`
+        }
+    }
+
+
+    //Event Listener for Theme Toggle
+    document.getElementById('theme-toggle').addEventListener('click', switchTheme)
+});
