08fe5c24a94bc1937f687d01dd92f2686185f4f4
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-05-07 21:41:45 +0530

    Updated navbar

--- js/navbarOps.js
+++ js/navbarOps.js
@@ -17,12 +17,34 @@ document.addEventListener('DOMContentLoaded', () => {
             document.documentElement.style.setProperty('--primary-color', '#1f1f1f')
             document.documentElement.style.setProperty('--secondary-color', '#e0e0e0')
 
-            //Switching the theme icon to 'light'
+            //Switching the theme icon to 'ligt'
             document.getElementById('theme-toggle').innerHTML = `<i class='bx bx-sun'></i>`
         }
     }
 
+    //Function to pull menu
+    function pullMenu() {
 
-    //Event Listener for Theme Toggle
+        //Animation for pulling the menu
+        gsap.to(".responsive-navbar-container", {height: '100vh', duration: 0.1, ease: 'power1.in'})
+    }
+
+    //Function to push menu
+    function pushMenu() {
+
+        //Animation for pushing the menu
+        gsap.to(".responsive-navbar-container", {height: '0', duration: 0.1, ease: 'power1.in'})
+    }
+
+
+    //EVENT LISTENERS
+
+    //Theme Toggle
     document.getElementById('theme-toggle').addEventListener('click', switchTheme)
+
+    //Pulling menu
+    document.getElementById('burger-menu').addEventListener('click', pullMenu)
+
+    //Pushing menu
+    document.getElementById('close-resp-menu').addEventListener('click', pushMenu)
 });
