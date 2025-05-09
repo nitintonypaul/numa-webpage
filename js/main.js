442988cd169e38279326262ad70167f1f9bc2d63
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-05-09 12:43:39 +0530

    webpage complete

--- js/main.js
+++ js/main.js
@@ -14,9 +14,17 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.from('.intro-image-container', {y:10, opacity: 0, duration: 0.7, delay: 1.1})
 
     //Scrolling Animation for Focus and Flow section
-    gsap.from(".ff-heading", {y: 50, opacity: 0, duration: 1, scrollTrigger: { trigger: ".ff-heading", start: "top 80%", toggleActions: "play none none none"}});
-    gsap.from(".ff-image-container", {y: 50, opacity: 0, duration: 0.7, scrollTrigger: { trigger: ".ff-image-container", start: "top 80%", toggleActions: "play none none none"}});
-    gsap.from(".ff-description", {y: 50, opacity: 0, duration: 0.7, delay:0.2, scrollTrigger: { trigger: ".ff-description", start: "top 80%", toggleActions: "play none none none"}});
+    gsap.from(".ff-heading", {y: 50, opacity: 0, duration: 1, scrollTrigger: { trigger: ".ff-heading", start: "top 70%", toggleActions: "play none none none"}});
+    gsap.from(".ff-image-container", {y: 50, opacity: 0, duration: 0.7, scrollTrigger: { trigger: ".ff-image-container", start: "top 70%", toggleActions: "play none none none"}});
+    gsap.from(".ff-description", {y: 50, opacity: 0, duration: 0.7, delay:0.2, scrollTrigger: { trigger: ".ff-description", start: "top 70%", toggleActions: "play none none none"}});
 
-    
+    //Scrolling Animation for Modular section
+    gsap.from(".m-heading", {y: 50, opacity: 0, duration: 1, scrollTrigger: { trigger: ".m-heading", start: "top 70%", toggleActions: "play none none none"}});  
+    gsap.from(".m-image-container", {y: 50, opacity: 0, duration: 1, scrollTrigger: { trigger: ".m-image-container", start: "top 70%", toggleActions: "play none none none"}});  
+    gsap.from(".m-description", {y: 50, opacity: 0, duration: 1, scrollTrigger: { trigger: ".m-description", start: "top 70%", toggleActions: "play none none none"}});  
+
+    //Scrolling Animation for Open Source section
+    gsap.from(".o-heading", {y: 50, opacity: 0, duration: 1, scrollTrigger: { trigger: ".o-heading", start: "top 70%", toggleActions: "play none none none"}});
+    gsap.from(".o-logo-container", {y: 50, opacity: 0, duration: 1, scrollTrigger: { trigger: ".o-logo-container", start: "top 80%", toggleActions: "play none none none"}});  
+    gsap.from(".o-description", {y: 50, opacity: 0, duration: 1, delay: 0.2, scrollTrigger: { trigger: ".o-description", start: "top 80%", toggleActions: "play none none none"}});
 })
--- js/navbarOps.js
+++ js/navbarOps.js
@@ -2,31 +2,51 @@
 //Ensuring DOMContent is loaded
 document.addEventListener('DOMContentLoaded', () => {
 
+
+    //FUNCTIONS
+
+    //Function to set the theme to dark
+    function setDark() {
+        document.documentElement.style.setProperty('--primary-color', '#1f1f1f')
+        document.documentElement.style.setProperty('--secondary-color', '#e0e0e0')
+
+        //Switching the theme icon to 'ligt'
+        document.getElementById('theme-toggle').innerHTML = `<i class='bx bx-sun'></i>`
+
+        //Changing intro image
+        document.getElementById('intro-image-container').innerHTML = `<img src="assets/numa-light.PNG">`
+
+        //Updating localStorage with current theme
+        localStorage.setItem('theme','dark')
+    }
+
+    //Function to set the theme to light
+    function setLight() {
+        document.documentElement.style.setProperty('--primary-color', '#e0e0e0')
+        document.documentElement.style.setProperty('--secondary-color', '#1f1f1f')
+
+        //Switching the theme icon to 'dark'
+        document.getElementById('theme-toggle').innerHTML = `<i class='bx bx-moon'></i>`
+
+        //Changing intro image
+        document.getElementById('intro-image-container').innerHTML = `<img src="assets/numa-dark.PNG">`
+
+        //Updating localStorage with current theme
+        localStorage.setItem('theme','light')
+    }
+
     //Function to switch theme
     function switchTheme() {
 
         //Checking if the theme is dark
         if (getComputedStyle(document.documentElement).getPropertyValue('--primary-color').trim() == '#1f1f1f') {
-            document.documentElement.style.setProperty('--primary-color', '#e0e0e0')
-            document.documentElement.style.setProperty('--secondary-color', '#1f1f1f')
-
-            //Switching the theme icon to 'dark'
-            document.getElementById('theme-toggle').innerHTML = `<i class='bx bx-moon'></i>`
-
-            //Changing intro image
-            document.getElementById('intro-image-container').innerHTML = `<img src="assets/numa-dark.PNG">`
+            setLight()
         }
         else {
-            document.documentElement.style.setProperty('--primary-color', '#1f1f1f')
-            document.documentElement.style.setProperty('--secondary-color', '#e0e0e0')
-
-            //Switching the theme icon to 'ligt'
-            document.getElementById('theme-toggle').innerHTML = `<i class='bx bx-sun'></i>`
-
-            //Changing intro image
-            document.getElementById('intro-image-container').innerHTML = `<img src="assets/numa-light.PNG">`
+            setDark()
         }
     }
+
 
     //Function to pull menu
     function pullMenu() {
@@ -35,11 +55,27 @@ document.addEventListener('DOMContentLoaded', () => {
         gsap.to(".responsive-navbar-container", {height: '100vh', duration: 0.1, ease: 'power1.in'})
     }
 
+    
     //Function to push menu
     function pushMenu() {
 
         //Animation for pushing the menu
         gsap.to(".responsive-navbar-container", {height: '0', duration: 0.1, ease: 'power1.in'})
+    }
+
+
+    //FUNCTIONALITY
+
+    //Checking if the user has selected a theme before using localStorage
+    if (localStorage.getItem('theme') !== null) {
+        localStorage.getItem('theme') == 'dark' ? setDark() : setLight()
+    } 
+    //Checking preferred theme if the user has not selected a theme before
+    else if (window.matchMedia('(prefers-color-scheme: dark').matches) {
+        setDark()
+    }
+    else {
+        setLight()
     }
 
 
@@ -53,4 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Pushing menu
     document.getElementById('close-resp-menu').addEventListener('click', pushMenu)
+
+    //If the user clicks 'Home' on the responsive menu
+    document.getElementById('resp-home').addEventListener('click', pushMenu)
 });
